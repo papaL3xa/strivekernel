0dc188d42d6624bd418e37c86a0281ad99a360c8
papaL3xa <john.scriven03@gmail.com>
2025-11-21 16:33:52 +0700

    fix build: rename build.sh in workflow

--- build.sh
+++ build.sh
@@ -287,7 +287,7 @@ setup_localversion() {
     
     KERNEL_DEFCONFIG="exynos9820-${DEVICE}_defconfig"
     local defconfig="${RDIR}/arch/arm64/configs/${KERNEL_DEFCONFIG}"
-    local localversion="-SmurfKernel_${MODEL}-${BUILD_KERNEL_VERSION}-A${PLATFORM_VERSION}"
+    local localversion="-SmurfKernel-${MODEL}-${BUILD_KERNEL_VERSION}-A${PLATFORM_VERSION}"
     
     # Check if defconfig exists
     if [[ ! -f "$defconfig" ]]; then
